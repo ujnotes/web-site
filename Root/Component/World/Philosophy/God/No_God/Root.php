d8842f532d83552f6d37add0f342ec96e1b12c11
Ujjwal Singh <ujjwalsingh@outlook.com>
2016-11-16 07:36:10 +0530

    Published 'god', Added social links, Updated roadmap and sitemap

--- Root/Component/World/Philosophy/God/No_God/Root.php
+++ Root/Component/World/Philosophy/God/No_God/Root.php
@@ -1,52 +1,57 @@
 <div class='content'>
 	<div class='message'>
     <p>
-I am an ordinary person, my name just like my face is not special. The only thing special here are the things I share with you. like this human form of ours, that we were both born on this same planet, now alive on this very same day. I am - your brother and we all are one big family.
-I need to tell you something very important. I need to tell you that you have been lied to. There is no God, not one not many not any.
-Thus there also are no messengers of God, or books of God. We have been living amidst a war! On one side you have religious books that you are not allowed to question along with men who want to control you and the rest of the world exploiting your weaknesses with those books and on the other side are common people, born into different communities, like you and me. Dear friends belonging to any community, I urge you to please stop letting these people take over your mind. We are all equal and there is no God, any God.
-How can I make such a claim, you may ask? Let me explain.
-Why do you believe in a God? because someone told you referring a book. Well some one told ISIS that they should kill us all referring to the same or a similar book.
-This 'belief' business is too dangerous, just look at where it has got us today. Just how many of us it has got killed.
-Your religion is arbitrary just like your name. It was decided by your parents.
-The problem is we join these belief cults without much choice, at an early age when we are not capable of reason. Just like a taste, religion becomes an acquired habit.
-In order to avoid conflict we choose to look away from other beliefs, and let them be, so that they can let us be.
-But let me give you an example: what if tomorrow some one begins 'believing' that they should kill every one whose names start with a particular alphabet, as some one planted what they now believe is a 'holy book', commanding so, in their backyard?
-Those people believing that - might not choose to act on it, but they will continue to pass on that belief to other people, write books and songs and plays about it. Any one of those that chooses to act eventually, or by incentives is misguided to act, will result in unwarranted and unnecessary violence. More importantly this can be easily exploited by third parties for their own interests, as happens in the proxy wars.
-Religion besides its perks is a weapon of mass degradation and destruction. This has been quite visible through out the history to this day.
-When you see the videos of war and beheading from Syria and Iraq do you see any God saving them magically or do you see devils killing with lightning bolts, or is it just human beings killing other human beings?
-Has some holy book been found on Mars or the Moon?
-You may now ask: then how did we come into existence, how we got our breath.
-Let me tell you this, it does not matter how we got it, it does not matter who created this world.
-When compared to the bigger questions like what do we do to stop this violence, how can we have global peace?
-What matters more: What I had for dinner today or what I have been telling you all this while.
-The question of our origin, is only a matter of past mostly, a subject of scientific research now that we have science and not speculation.
-What matters is the truth that there is no God like character to be seen around.
-God is no where. If he was, he would probably be enjoying a 'vacation' somewhere or it would be that he enjoys watching this suffering and killing? Would you let one of your child burn your other child to death? Or is it that he does not exist?
-If he was there, would he not save the children and women and innocents you have been watching dying for no reason.
-If you have a theory for that than you can have a garbage self-centered selfish theory for anything.
-It is our brothers and sisters who are getting raped and killed every single day in Syria and Iraq. And the excuse used is: I am better than you, for my god is holier than yours. Madness!
-If only people in Syria had understood that there is no God to divide them, Shia and Sunni are one, that each should treat the other as equal. There would be no need for a war.
-Imagine if ISIS learnt - like absorbed this single piece of knowledge: that there is no God, that they have been lied to or that they have lied to themselves, that there is no hell or heaven, would they still kill on that scale?
-If only we knew we are all one, that there are no Gods, nothing to divide us, perhaps we could have some sort of a heaven here.
-We must stop inheriting and passing on this lie. We must stop using it as an excuse to hate and automatically elevate ourselves.
-They have taken the control of our minds, exploited our weaknesses of fear and uncertainty and implanted their fairy tales.
-You don't need to fear hell and long for heaven like 5 year old kids that are scared of monsters and cry for a candy. You can still be good, for you know what 'good' is.
-Perhaps with an extremely few exceptions, most of you would indeed help a drowning child if you came across one. Do you need a book to tell you that? Even if you did why does it have to come from a God.
-Next, death has been another of our great weaknesses. The lack of knowledge as to what happens after death.
-Let me tell you: when we die, its just like taking off the battery from a toy or a mobile phone. Sorry, but nothing happens after that. But that is all the more reason to live to the fullest, doing good, while we are still alive.
-Look at your eyes aren't they special aren't they magical, look at your face in the mirror aren't you beautiful. The only missing component might be goodness, in and around you.
-'God' is just and idea, it is the union of goodness in you and me. Together we realize a 'God' by doing good work. That's it, no supernatural being is needed.
-My request to the people in Iraq and Syria is, please know that there is no god, heaven and hell. Every person that you are killing and torturing is your own brother or sister.
-If there is any other way than killing then please walk on that path instead. Even if you must kill at least stop using the name of God and your sect, or else history will continue to repeat itself.
-We all have less than a 100 years or so to live, you and I will be dead, but these wars will wage on, unless we stop it.
-We need to free the minds from the fear of hell. We need to make it clear that there is no heaven or god.
-This here is all that we have.
-We need to set a date, on which humanity collectively concludes that there is no God.
-I wonder if that day will be today, or how many more genocides and beheading and rapes it will take.
-But let me tell you this whether I or you will be alive to see that day or not, it will happen. Why I am so sure, because it is the natural way of beings, if we can - the majority of us avoid violence. And we will have the means to avoid it.
-Meanwhile, humans of the future will look back to this moment in time at us and watch who stood for God and who stood for humanity.
-I will not see you in some ‘heaven’, I wish I see you healthy and happy right here.
+			I am an ordinary person, my name just like my face is not special. The only thing special here are the things I share with you. like this human form of ours, that we were both born on this same planet, now alive on this very same day. I am - your brother and we all are one big family.
+			I need to tell you something very important. I need to tell you that you have been lied to. There is no God, not one not many not any.
+			Thus there also are no messengers of God, or books of God. We have been living amidst a war! On one side you have religious books that you are not allowed to question along with men who want to control you and the rest of the world exploiting your weaknesses with those books and on the other side are common people, born into different communities, like you and me. Dear friends belonging to any community, I urge you to please stop letting these people take over your mind. We are all equal and there is no God, any God.
+			How can I make such a claim, you may ask? Let me explain.
+		</p>
+		<p>
+			Why do you believe in a God? because someone told you referring a book. Well some one told ISIS that they should kill us all referring to the same or a similar book.<br>
+			This 'belief' business is too dangerous, just look at where it has got us today. Just how many of us it has got killed.<br>
+			Your religion is arbitrary just like your name. It was decided by your parents.<br>
+			The problem is we join these belief cults without much choice, at an early age when we are not capable of reason. Just like a taste, religion becomes an acquired habit.
+			In order to avoid conflict we choose to look away from other beliefs, and let them be, so that they can let us be.
+			But let me give you an example: what if tomorrow some one begins 'believing' that they should kill every one whose names start with a particular alphabet, as some one planted what they now believe is a 'holy book', commanding so, in their backyard?
+			Those people believing that - might not choose to act on it, but they will continue to pass on that belief to other people, write books and songs and plays about it. Any one of those that chooses to act eventually, or by incentives is misguided to act, will result in unwarranted and unnecessary violence. More importantly this can be easily exploited by third parties for their own interests, as happens in the proxy wars.
+			Religion besides its perks is a weapon of mass degradation and destruction. This has been quite visible through out the history to this day.
+			When you see the videos of war and beheading from Syria and Iraq do you see any God saving them magically or do you see devils killing with lightning bolts, or is it just human beings killing other human beings?
+			Has some holy book been found on Mars or the Moon?
+			You may now ask: then how did we come into existence, how we got our breath.
+			Let me tell you this, it does not matter how we got it, it does not matter who created this world.
+			When compared to the bigger questions like what do we do to stop this violence, how can we have global peace?
+			What matters more: What I had for dinner today or what I have been telling you all this while.
+			The question of our origin, is only a matter of past mostly, a subject of scientific research now that we have science and not speculation.
+			What matters is the truth that there is no God like character to be seen around.
+			God is no where. If he was, he would probably be enjoying a 'vacation' somewhere or it would be that he enjoys watching this suffering and killing? Would you let one of your child burn your other child to death? Or is it that he does not exist?
+			If he was there, would he not save the children and women and innocents you have been watching dying for no reason.
+			If you have a theory for that than you can have a garbage self-centered selfish theory for anything.
+			It is our brothers and sisters who are getting raped and killed every single day in Syria and Iraq. And the excuse used is: I am better than you, for my god is holier than yours. Madness!
+			If only people in Syria had understood that there is no God to divide them, Shia and Sunni are one, that each should treat the other as equal. There would be no need for a war.
+			Imagine if ISIS learnt - like absorbed this single piece of knowledge: that there is no God, that they have been lied to or that they have lied to themselves, that there is no hell or heaven, would they still kill on that scale?
+			If only we knew we are all one, that there are no Gods, nothing to divide us, perhaps we could have some sort of a heaven here.
+			We must stop inheriting and passing on this lie. We must stop using it as an excuse to hate and automatically elevate ourselves.
+			They have taken the control of our minds, exploited our weaknesses of fear and uncertainty and implanted their fairy tales.
+			You don't need to fear hell and long for heaven like 5 year old kids that are scared of monsters and cry for a candy. You can still be good, for you know what 'good' is.
+			Perhaps with an extremely few exceptions, most of you would indeed help a drowning child if you came across one. Do you need a book to tell you that? Even if you did why does it have to come from a God.
+			Next, death has been another of our great weaknesses. The lack of knowledge as to what happens after death.
+			Let me tell you: when we die, its just like taking off the battery from a toy or a mobile phone. Sorry, but nothing happens after that. But that is all the more reason to live to the fullest, doing good, while we are still alive.
+			Look at your eyes aren't they special aren't they magical, look at your face in the mirror aren't you beautiful. The only missing component might be goodness, in and around you.
+			'God' is just and idea, it is the union of goodness in you and me. Together we realize a 'God' by doing good work. That's it, no supernatural being is needed.
+			My request to the people in Iraq and Syria is, please know that there is no god, heaven and hell. Every person that you are killing and torturing is your own brother or sister.
+			If there is any other way than killing then please walk on that path instead. Even if you must kill at least stop using the name of God and your sect, or else history will continue to repeat itself.
+			We all have less than a 100 years or so to live, you and I will be dead, but these wars will wage on, unless we stop it.
+			We need to free the minds from the fear of hell. We need to make it clear that there is no heaven or god.
+			This here is all that we have.
+			We need to set a date, on which humanity collectively concludes that there is no God.
+			I wonder if that day will be today, or how many more genocides and beheading and rapes it will take.
+			But let me tell you this whether I or you will be alive to see that day or not, it will happen. Why I am so sure, because it is the natural way of beings, if we can - the majority of us avoid violence. And we will have the means to avoid it.
+			Meanwhile, humans of the future will look back to this moment in time at us and watch who stood for God and who stood for humanity.
+			I will not see you in some ‘heaven’, I wish I see you healthy and happy right here.
     </p>
+		<p>
+			If all became atheists overnight, how would it change the world
+		</p>
 	</div>
 	<?php require('Fragment\Component_bottom.php') ?>
 </div>
