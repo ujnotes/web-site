113966b6feea76eb501ad07efa31ac8fde98d872
Ujjwal Singh <ujjwalsingh@outlook.com>
2016-12-11 05:15:24 +0530

    Author meta tag (general (fb) + twitter), Article: No_god

--- Root/Framework/Fragment/Twitter_Meta.php
+++ Root/Framework/Fragment/Twitter_Meta.php
@@ -3,3 +3,4 @@
 <meta name="twitter:title" content="<?php echo getComponentTitle($id) ?>">
 <meta name="twitter:description" content="<?php echo getComponentDesc($id) ?>">
 <meta name="twitter:image" content="<?php echo $config['base_url'].'/'.$id ?>.jpg">
+<meta name="twitter:creator" content="<?php echo $config['twitter_author'] ?>.jpg">
