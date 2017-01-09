1127d5d1fb6cc7801a0381f9d21a3aae1b49f802
Ujjwal Singh <ujjwalsingh@outlook.com>
2017-01-09 19:59:20 +0530

    Added 'Problem'

---
+++ Root/Component/World/Philosophy/Problem.php
@@ -0,0 +1,42 @@
+<div class='content'>
+	<div class='message'>
+		<?php $alt='A curved earth in space'; require('Fragment\Component_cover.php') ?>
+		<h3>
+			What is a problem?
+		</h3>
+		<p>
+			A matter or situation that is impeding or harmful and thus needing to be resolved.
+		</p>
+		<h3>
+			The problems of this world:
+		</h3>
+		<ol>
+			<li>Ignorance and stupidity of man</li>
+		</ol>
+		<p>
+			Things like crime, poverty, hunger, environmental destruction and animal cruelty are mostly but a consequence of the first problem.<br>
+			And the rest, like cancer and death are only 'realities'. If curable they are only 'tasks' to be accomplished.<br>
+			Which again are impeded by the first problem.
+		</p>
+		<h3>
+			&diams;
+		</h3>
+		<p>
+			Man, by his nature seeks to control and thus dominate what is around him.<br>
+			And so, many simply 'like' to discriminate and kill.<br>
+			Now that our civilization is thousands of years old, we already have the knowledge, the resources and the tools.<br>
+			Yet man wants to remain selfish even though it is understood, how futile and limited his personal life is.<br>
+			Because of this, he continues to 'waste' his life on trivial things; Often hurting others on his way.<br>
+			Due to his hunger for 'more' and his eccentricity, that wanting of being 'special' - he is readily sold to irrational ideas.<br>
+			He thus often subscribes to the idea of a personal 'god' that 'cares' about him 'personally'.<br>
+			Racism and sectarianism rise from the same selfish craving of wanting to stand above the rest and dominate.<br>
+			The structure of our societies and its economy exploit and thus promote this eccentric behavior.<br>
+			While morality is often compromised over personal gains, life is reduced to sell and buy then show and throw and its repetition.<br>
+			At the level of sects, races and then countries this eccentricity continues and periodically results in violent eruptions.<br>
+			Even to this day, world peace dangles precariously on the prospect of mutually assured destruction.<br>
+			<br>
+			All because man refuses to learn and embrace what has already been learnt.
+		<p>
+	</div>
+	<?php require('Fragment\Component_bottom.php') ?>
+</div>
