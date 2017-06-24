9bcba47f6489c32ffcf4b84df17feea3470ed299
Ujjwal Singh <ujjwalsingh@outlook.com>
2017-06-24 21:42:47 +0530

    Home text

--- Root/HTML/Component/World/Philosophy/Consciousness/Index.php
+++ Root/HTML/Component/World/Philosophy/Consciousness/Index.php
@@ -1,15 +1,24 @@
 <div class='message'>
 	<?php $alt='A man thinking looking at the sunrise'; require('..\HTML\Fragment\Component_cover.php') ?>
+	<h2 class='center'>Understanding 'I' - the consciousness</h2>
+	<p>
+		We are a part of this world, and made of the same matter as the rest of it. Yet there is something profoundly different about us.<br>
+		We are the living and thinking ones. Conscious and aware of our own existence. We 'decide' and choose our actions.<br>
+		All thanks to our complex brains<br>
+		But are we really that 'special', are we really that 'free'?<br>
 	<h3>Thoughts</h3>
 	<p>
+		A thought is a virtual partial simulation of reality. Evolution devised it for survival.<br>
+		E.g. a sound of the _ of dried leaves triggers a thought of being prayed by a larger predator thereby making the individual more active and less prone to becoming someone's snack.<br>
 	</p>
 	<h3>Self awareness</h3>
 	<p>
+		A system capable of thoughts that is aware of its own existence in that it knows of its physical extents.
+		
 	</p>
 	<h3>Consciousness</h3>
 	<p>
 		Being able to think what to think.
-		That 'I'. What is it?
 	</p>
 </div>
 <?php require('..\HTML\Fragment\Component_bottom.php') ?>
