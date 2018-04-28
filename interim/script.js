9d2f4ea690819a553dc5c4cad170c5101aa9f014
Ujjwal Singh <ujjwalsingh@outlook.com>
2018-04-28 23:05:52 +0530

    Publish

--- interim/script.js
+++ interim/script.js
@@ -205,8 +205,7 @@ function executeReload(startTime, resp, target) {
 		var height = document.getElementById('canvas-main').scrollHeight;
 		document.getElementById('nav-menu').style.maxHeight = height+'px';
 		document.getElementById('canvas-main').style.maxHeight = null;
-		if(resp.xurl == '1')
-			setXURL(document);
+		setXURL(document);
 		if(resp.async == '1')
 			initPageFunction(target);
 		fbReload();
@@ -496,8 +495,7 @@ window.onload = function() {
 
 	menu_button.addEventListener( 'click', function() {
 		if (!menuActive) {
-			window.history.pushState({'id':'menu'}, '', '/'+'menu');
-			activateMenuFn();
+			activateMenu();
 		}
 		else {
 			if(curTab == 'root' || curTab == 'menu')
@@ -610,6 +608,11 @@ var activateMenuFn = function() {
 	}
 }
 
+var activateMenu = function() {
+	window.history.pushState({'id':curTab}, '', '/'+curTab);
+	activateMenuFn();
+}
+
 var activateMainFn = function() {
 	var main_wrapper = document.querySelector( '#main-wrapper' ),
 		menu_button = document.querySelector( '#menu-button' );
@@ -618,6 +621,12 @@ var activateMainFn = function() {
 	classie.remove( menu_button, 'active' );
 	menuActive = false;
 }
+
+var activateMain = function() {
+	window.history.pushState({'id':curTab}, '', '/'+curTab);
+	activateMainFn();	
+}
+
 function getURLid() {
 	var loc = window.location.pathname;
 	if(loc == '/')
@@ -638,8 +647,10 @@ function setXURL(node) {
 	var arClassElement = getElementsByClassName(node, 'XURL');
 	var n = arClassElement.length;
 	for(i = 0; i < n; i++) {
-		var a = arClassElement[i].children[0];
-		arClassElement[i].onclick = loadCanvasI;
+		if(arClassElement[i].getAttribute('data-target') == 'menu')
+			arClassElement[i].onclick = activateMenu;
+		else
+			arClassElement[i].onclick = loadCanvasI;
 	}
 }
 function root() {
