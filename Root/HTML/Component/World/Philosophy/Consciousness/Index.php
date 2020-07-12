deb35f1f707c94ec232d6e2bcbf25a5187b932d5
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-07-12 06:17:11 +0530

    Added /world/philosophy/consciousness

--- Root/HTML/Component/World/Philosophy/Consciousness/Index.php
+++ Root/HTML/Component/World/Philosophy/Consciousness/Index.php
@@ -2,23 +2,33 @@
 	<?php $alt='A man thinking looking at the sunrise'; require('..\HTML\Fragment\Component_cover.php') ?>
 	<h2 class='center'>Understanding 'I' - the consciousness</h2>
 	<p>
-		We are a part of this world, and made of the same matter as the rest of it. Yet there is something profoundly different about us.<br>
+		We are a part of this world, and made up of the same matter as the rest of it. Yet there is something profoundly different about us.<br>
 		We are the living and thinking ones. Conscious and aware of our own existence. We 'decide' and choose our actions.<br>
-		All thanks to our complex brains<br>
-		But are we really that 'special', are we really that 'free'?<br>
-	<h3>Thoughts</h3>
+		All thanks to our complex brains.<br>
+		<br>
+		But are we really that 'special', are we really that 'free'? Let's examine.
+	</p>
+	<h3>What are thoughts?</h3>
 	<p>
 		A thought is a virtual partial simulation of reality. Evolution devised it for survival.<br>
-		E.g. a sound of the _ of dried leaves triggers a thought of being prayed by a larger predator thereby making the individual more active and less prone to becoming someone's snack.<br>
+		E.g. a sight of predator charging towards oneself triggers a 'thought' of being eaten alive by it next - thereby making the individual act in order to save its life.<br>
 	</p>
-	<h3>Self awareness</h3>
+	<h3>What is self-awareness?</h3>
 	<p>
-		A system capable of thoughts that is aware of its own existence in that it knows of its physical extents.
-		
+		Thoughts that are aware of its own existence - the simulator, - can be said to be "self-aware".<br>
+		<br>
+		As such a self-aware system has a sense of the 'I'. In other words, it can simulate itself.<br>
+		E.g. a self-driving car's auto pilot software will include itself in the drive simulation. Besides its environment, it will have knowledge about its own extents and position and characteristics.<br>
+		If it also knows that the one running the simulation is the same as its image in the simulation then it could be said to be self-aware.
 	</p>
-	<h3>Consciousness</h3>
+	<h3>What is consciousness?</h3>
 	<p>
-		Being able to think what to think.
+		Consciousness is the ability to be able to think - what to think. A conscious system is one that is self-aware and self-directing in that it can select what to think upon.<br>
+		As such, it does not take direct instructions as to what to think about - from an external agency. It is the 'master' program, tasked with the role of managing thoughts and routing information and actions.
+	</p>
+	<h3>What is a mind?</h3>
+	<p>
+		A mind is that software platform on which the thought-simulations run.
 	</p>
 </div>
 <?php require('..\HTML\Fragment\Component_bottom.php') ?>
