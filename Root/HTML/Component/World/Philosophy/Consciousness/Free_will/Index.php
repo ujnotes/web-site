748f07535902289ad9d32499cb40fed1f4f8c03d
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-07-29 04:00:12 +0530

    Added article: "Free will"

---
+++ Root/HTML/Component/World/Philosophy/Consciousness/Free_will/Index.php
@@ -0,0 +1,86 @@
+<div id='message'>
+	<?php $alt='A road'; require('..\HTML\Fragment\Component_cover.php') ?>
+	
+	<h3>What is free will?</h3>
+	<p>
+		Free will is the ability to choose a course of action independent of prior events.<br>
+		E.g. imagine a series of dominos. What happens to the next domino is determined, other than the inherent static state of that system - like the mass and position of the dominos, entirely by the action of the domino just beside it.<br>
+		If we have all the required information of any moment, we can predict exactly what will happen next.<br>
+		But say, if a domino was free to decide and choose on its own, what it would do next amongst a set of more than one possibilities - independent of the events before it, we would never be able to deterministically assert; and thus it would be said to have free will.<br>
+	</p>
+	
+	<h3>Do we have free will?</h3>
+	<p>
+		Are we 'free', to think and decide and choose?
+		Or is it that we are merely acting out the choices that have already been made.
+	</p>
+	<p>
+		A noteworthy example is of two calculators of the exact same make. If one turns them both on and asks them to generate random numbers, they both will generate the same numbers in sequence.<br>
+		As such, the numbers they generate are in fact pseudo-random numbers.<br>
+		It turns out we ourselves could be - like those dumb calculators.<br>
+		Another important example is of the answers to questions like one's favorite dish or colour or car.<br>
+		A subject can 'only' answer with a case - that he has prior to that point, heard or seen.
+	</p>
+	<p>
+		The past DETERMINES the future... We only observe it, we only observe the act of choosing - but the choice perhaps has already been made.<br>
+		The issue here is that our brain itself is part of this world - and functions on the parameters of the world.<br>
+		So while our act of observing ourselves making a choice might be 'real', the whole 'scene' though - could simply be predetermined and preordained.
+	</p>
+	<p>
+		The big question is: if we were to ever run a computer simulation of our 'existence' with the exact same initial state of our world, could we, without altering anything thing midway,
+		in that simulation reach the exact state as that of the present reality?<br>
+		If so then it would conclude that there is no free will; only its illusion.
+	</p>
+	
+	<h3>Is free will, then merely an illusion?</h3>
+	<p>
+		While it does appear to us that we can choose either of the choices we are aware of, the act of choosing one though does seem to be already determined.<br>
+		Let's take another example of choosing to bat or bowl first with the flip of a coin. While the outcome of this coin flip may appear to be completely random - but is in fact again determined by the prior leading events.<br>
+		Thus it could be said that free will is only an illusion.
+	</p>
+	
+	<h3>Are we free from our actions and not responsible for it?</h3>
+	<p>
+		Perhaps no - we are not free! It does not matter - that it does not matter. Since one will be playing out the part of making the choice and facing the consequences anyways; even if the choice has already been made for them...
+	</p>
+	<p>
+		In short - there is NO escape, regardless!
+		No matter what, one cannot stand in front of a train - screaming that "the choice has already been made". One has TO -- LIVE IT!
+	</p>
+	
+	<h3>What is the morality of punishment?</h3>
+	<p>
+		If there is no free will, how responsible is the criminal for the crime he could have not - not done? As would be the case if we indeed had no free will.<br>
+		But then within the "canned" experience of reality
+	</p>
+	
+	<h3>Should we then worry about the outcomes?</h3>
+	<p>
+		Perhaps the outcome has already been chosen.<br>
+		Still, we must "want" the outcome to be a certain way, only then will we follow down a path that makes it happen. So it goes both ways - we should worry and not!<br>
+		In that, while we should focus on the goal, and worry about the result only to that extent where it guides us on to the path necessary to achieve the goal. Worrying beyond that is both pointless and detrimental as it could make us deviate from the necessary path.<br>
+		While worrying about the outcome 'has' a role in the outcome that is yet to materialize, knowledge of the free will illusion means also that we know: neither can we change the past, nor we really ever could...<br>
+		<br>
+		This also goes quite in line with the teaching of the Gita &mdash; the prominent scripture of the Hindus (a book written by men, like EVERY other book) - that says:<br>
+		"Worry not about the result, only about your deeds".<br>
+		Precisely: 
+	</p>
+	<blockquote cite="http://gitabase.com/the/gita/eng/BG/2/47">You have a right to perform your prescribed duty, but you are not entitled to the fruits of action. Never consider yourself the cause of the results of your activities, and never be attached to not doing your duty
+	&nbsp; &mdash; <a class='content-link' href="http://gitabase.com/the/gita/eng/BG/2/47" target='_blank'>Gita : 2.47</a></blockquote>
+	
+	<h3>So then - what is all this?</h3>
+	<p>
+		It is likely that what we are experiencing - is but only a 'playback' of a prerecorded "movie".<br>
+		I think the concept of - &ldquo;<a class='content-link' href="https://en.wikipedia.org/wiki/Maya_(religion)" target='_blank'>Maya</a>&rdquo; &mdash; an illusion, from Hindu philosophy comes closest to this!
+	</p>
+	
+	<h3>Ok, but then who is the 'director' of that movie?</h3>
+	<p>
+		You.<br>
+		Perhaps!<br>
+		The director and the movie seem to be merged - inseparable!<br>
+		Again, this is addressed in the Hindu philosophy by the concept of non-duality - &ldquo;<a class='content-link' href="https://en.wikipedia.org/wiki/Advaita_Vedanta" target='_blank'>Advaita</a>&rdquo;
+	</p>
+	
+</div>
+<?php require('..\HTML\Fragment\Component_bottom.php') ?>
