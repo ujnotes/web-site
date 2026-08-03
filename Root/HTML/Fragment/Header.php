6a50fbe83e3f9b6416d46f185691925f529ff0ca
Ujjwal Singh <ujjwalsingh@outlook.com>
2026-08-03 11:09:46 +0530

    Scale localized header slogans

--- Root/HTML/Fragment/Header.php
+++ Root/HTML/Fragment/Header.php
@@ -1,9 +1,13 @@
-<?php require_once __DIR__.'/UITranslation.php'; ?>
+<?php
+	require_once __DIR__.'/UITranslation.php';
+	$header_slogan_scale = (float) getUITranslation('my_notes_font_scale', $lang);
+	if ($header_slogan_scale <= 0) $header_slogan_scale = 1;
+?>
 <div id='header-wrapper'>
 	<div id='header-title'>
 		<div id='header-slogan' class='XURL' data-target='root' data-title=''>
 			<a id='header-slogan-href' href='/'>
-				<div id='header-slogan-text'><?php echo htmlspecialchars(getUITranslation('my_notes', $lang), ENT_QUOTES, 'UTF-8'); ?></div>
+				<div id='header-slogan-text' style='font-size: <?php echo $header_slogan_scale; ?>em'><?php echo htmlspecialchars(getUITranslation('my_notes', $lang), ENT_QUOTES, 'UTF-8'); ?></div>
 			</a>
 		</div>
 		<div id='header-logo' class='XURL' data-target='root' data-title=''>
