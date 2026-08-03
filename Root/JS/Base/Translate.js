92f39b483b517a779e2906fcaacb3dc20941a2c9
Ujjwal Singh <ujjwalsingh@outlook.com>
2026-08-03 11:15:34 +0530

    Keep Google Translate off local hosts

---
+++ Root/JS/Base/Translate.js
@@ -0,0 +1,6 @@
+if (window.location.hostname === 'ujnotes.local' ||
+	window.location.hostname === 'localhost' ||
+	window.location.hostname.endsWith('.local') ||
+	window.location.hostname === '127.0.0.1') {
+	isTranslateButtonActive = false;
+}
