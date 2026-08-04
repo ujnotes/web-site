4cd37fa5191563e6703f62f4453e5a3dfb627b3b
Ujjwal Singh <ujjwalsingh@outlook.com>
2026-08-04 16:00:08 +0530

    Localize Hindi menu tiles

--- Root/HTML/Fragment/Menu.php
+++ Root/HTML/Fragment/Menu.php
@@ -1,5 +1,6 @@
 <?php
 	require_once 'Fragment/Item.php';
+	require_once __DIR__.'/UITranslation.php';
 	$SIDEBAR_NAV_GROUP = 'sidebar-nav-group page-list';
 ?>
 <div id='nav-menu'>
@@ -7,10 +8,10 @@
 		<div class='sidebar-nav-li sidebar-sub'>
 			<?php
 			$MENU_MAX_ITEM_COUNT = -5;
-			group_image($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['root', ''], ['world', 'World'], ['computer', 'Computer']);
-			group_text($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['about', 'About'], ['faq', 'FAQ']);
-			group_text($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['feedback', 'Feedback'], ['license', 'License'], ['logo', 'Logo'], ['roadmap', 'Roadmap'], ['changelog', 'Changelog'], ['timeline', 'Timeline']);
-			group_text($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['about_site', 'About site'], ['about_me', 'About me']);
+			group_image($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['root', getUITranslation('menu_home', $lang)], ['world', getUITranslation('menu_world', $lang)], ['computer', getUITranslation('menu_computer', $lang)]);
+			group_text($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['about', getUITranslation('menu_about', $lang)], ['faq', getUITranslation('menu_faq', $lang)]);
+			group_text($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['feedback', getUITranslation('menu_feedback', $lang)], ['license', getUITranslation('menu_license', $lang)], ['logo', getUITranslation('menu_logo', $lang)], ['roadmap', getUITranslation('menu_roadmap', $lang)], ['changelog', getUITranslation('menu_changelog', $lang)], ['timeline', getUITranslation('menu_timeline', $lang)]);
+			group_text($SIDEBAR_NAV_GROUP, $MENU_MAX_ITEM_COUNT, ['about_site', getUITranslation('menu_about_site', $lang)], ['about_me', getUITranslation('menu_about_me', $lang)]);
 			?>
 		</div>
 	</div>
