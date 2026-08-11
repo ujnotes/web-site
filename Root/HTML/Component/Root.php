0d28146ff7b38d2040cc537c772f43b99f099638
Ujjwal Singh <ujjwalsingh@outlook.com>
2026-08-12 00:36:21 +0530

    Build nested homepage tile hierarchy

--- Root/HTML/Component/Root.php
+++ Root/HTML/Component/Root.php
@@ -33,15 +33,16 @@
 </div>
 <div class='center' id='content-body-separator'></div>
 <div class='message_center_div' id='home-menu'>
-	<div class="home-menu-group-parent"><?php link_xurl('world', 'World') ?></div>
-	<?php group_image('page-list', 0, ['world/philosophy', 'Philosophy']); ?>
-	<?php
-	// '/universe' 'universe'
-	// 	'world/reality''Reality'
-	?>
-	<br>
-	<div class="home-menu-group-parent"><?php link_xurl('computer', 'Computer') ?></div>
-	<?php group_image('page-list', 1, ['computer/os', 'OS', '//ujnotes.com/computer/os'], ['computer/program', 'Program', '//ujnotes.com/computer/program'], ['computer/programming', 'Programming', '//ujnotes.com/computer/programming'], ['computer/game', 'Game']); ?>
+	<section class="home-menu-branch" aria-label="World">
+		<?php group_image('page-list home-menu-level home-menu-level-0', 0, ['world', 'World']); ?>
+		<?php group_image('page-list home-menu-level home-menu-level-1', 0, ['world/philosophy', 'Philosophy']); ?>
+		<?php group_image('page-list home-menu-level home-menu-level-2', 0, ...getSubComponents('world/philosophy')); ?>
+	</section>
+	<section class="home-menu-branch" aria-label="Computer">
+		<?php group_image('page-list home-menu-level home-menu-level-0', 0, ['computer', 'Computer']); ?>
+		<?php group_image('page-list home-menu-level home-menu-level-1', 1, ['computer/os', 'OS', '//ujnotes.com/computer/os'], ['computer/program', 'Program', '//ujnotes.com/computer/program'], ['computer/programming', 'Programming', '//ujnotes.com/computer/programming'], ['computer/game', 'Game']); ?>
+		<?php group_image('page-list home-menu-level home-menu-level-2', 0, ...getSubComponents('computer/game')); ?>
+	</section>
 </div>
 <div id='fb_components'>
 	<?php require('../HTML/Fragment/Component_FB_buttons.php') ?>
