30538a14eb86f870a3a24c993de50483e00936d7
Ujjwal Singh <ujjwalsingh@outlook.com>
2026-08-12 00:47:26 +0530

    Add collapsible homepage tile tree

--- Root/HTML/Component/Root.php
+++ Root/HTML/Component/Root.php
@@ -34,14 +34,34 @@
 <div class='center' id='content-body-separator'></div>
 <div class='message_center_div' id='home-menu'>
 	<section class="home-menu-branch" aria-label="World">
-		<?php group_image('page-list home-menu-level home-menu-level-0', 0, ['world', 'World']); ?>
-		<?php group_image('page-list home-menu-level home-menu-level-1', 0, ['world/philosophy', 'Philosophy']); ?>
-		<?php group_image('page-list home-menu-level home-menu-level-2', 0, ...getSubComponents('world/philosophy')); ?>
+		<div class="home-menu-node">
+			<button class="home-menu-toggle" type="button" aria-expanded="true" aria-controls="home-world-children" aria-label="Collapse World descendants" data-home-menu-toggle data-home-menu-label="World descendants"><span aria-hidden="true"></span></button>
+			<?php group_image('page-list home-menu-level home-menu-level-0', 0, ['world', 'World']); ?>
+		</div>
+		<div class="home-menu-subtree" id="home-world-children">
+			<div class="home-menu-node">
+				<button class="home-menu-toggle" type="button" aria-expanded="true" aria-controls="home-world-grandchildren" aria-label="Collapse Philosophy articles" data-home-menu-toggle data-home-menu-label="Philosophy articles"><span aria-hidden="true"></span></button>
+				<?php group_image('page-list home-menu-level home-menu-level-1', 0, ['world/philosophy', 'Philosophy']); ?>
+			</div>
+			<div class="home-menu-subtree" id="home-world-grandchildren">
+				<?php group_image('page-list home-menu-level home-menu-level-2', 0, ...getSubComponents('world/philosophy')); ?>
+			</div>
+		</div>
 	</section>
 	<section class="home-menu-branch" aria-label="Computer">
-		<?php group_image('page-list home-menu-level home-menu-level-0', 0, ['computer', 'Computer']); ?>
-		<?php group_image('page-list home-menu-level home-menu-level-1', 1, ['computer/os', 'OS', '//ujnotes.com/computer/os'], ['computer/program', 'Program', '//ujnotes.com/computer/program'], ['computer/programming', 'Programming', '//ujnotes.com/computer/programming'], ['computer/game', 'Game']); ?>
-		<?php group_image('page-list home-menu-level home-menu-level-2', 0, ...getSubComponents('computer/game')); ?>
+		<div class="home-menu-node">
+			<button class="home-menu-toggle" type="button" aria-expanded="true" aria-controls="home-computer-children" aria-label="Collapse Computer descendants" data-home-menu-toggle data-home-menu-label="Computer descendants"><span aria-hidden="true"></span></button>
+			<?php group_image('page-list home-menu-level home-menu-level-0', 0, ['computer', 'Computer']); ?>
+		</div>
+		<div class="home-menu-subtree" id="home-computer-children">
+			<div class="home-menu-node">
+				<button class="home-menu-toggle" type="button" aria-expanded="true" aria-controls="home-computer-grandchildren" aria-label="Collapse Game articles" data-home-menu-toggle data-home-menu-label="Game articles"><span aria-hidden="true"></span></button>
+				<?php group_image('page-list home-menu-level home-menu-level-1', 1, ['computer/os', 'OS', '//ujnotes.com/computer/os'], ['computer/program', 'Program', '//ujnotes.com/computer/program'], ['computer/programming', 'Programming', '//ujnotes.com/computer/programming'], ['computer/game', 'Game']); ?>
+			</div>
+			<div class="home-menu-subtree" id="home-computer-grandchildren">
+				<?php group_image('page-list home-menu-level home-menu-level-2', 0, ...getSubComponents('computer/game')); ?>
+			</div>
+		</div>
 	</section>
 </div>
 <div id='fb_components'>
--- Root/JS/Page/Root.js
+++ Root/JS/Page/Root.js
@@ -1,14 +1,31 @@
 function root() {
 	var e = document.getElementById('profile-image');
-	if(!e)
-		return;
-	root.full = false;
-	e.addEventListener( "click", function(){
-		if(root.full) {
-			e.blur();
-			root.full = false;
-		}
-		else
-			root.full = true;
+	if(e) {
+		root.full = false;
+		e.addEventListener( "click", function(){
+			if(root.full) {
+				e.blur();
+				root.full = false;
+			}
+			else
+				root.full = true;
+		});
+	}
+
+	[].forEach.call(document.querySelectorAll('#home-menu [data-home-menu-toggle]'), function(button) {
+		if(button.getAttribute('data-home-menu-initialized') == 'true')
+			return;
+
+		button.setAttribute('data-home-menu-initialized', 'true');
+		button.addEventListener('click', function() {
+			var target = document.getElementById(button.getAttribute('aria-controls'));
+			if(!target)
+				return;
+
+			var expanded = button.getAttribute('aria-expanded') == 'true';
+			button.setAttribute('aria-expanded', expanded ? 'false' : 'true');
+			button.setAttribute('aria-label', (expanded ? 'Expand ' : 'Collapse ') + button.getAttribute('data-home-menu-label'));
+			target.hidden = expanded;
+		});
 	});
 }
