c37da8e2a6d4d1648347cd2e3a3e246f0ebed710
Ujjwal Singh <ujjwalsingh@outlook.com>
2026-08-20 04:24:12 +0530

    Match the Hindi homepage to the English tree and pin short-page nav tiles to the footer.

--- Root/HTML/Component/hi/Root/index.php
+++ Root/HTML/Component/hi/Root/index.php
@@ -1,34 +1,53 @@
+<?php
+	require_once 'Fragment/Item_text.php';
+	require_once 'Fragment/Item_image.php';
+	require_once __DIR__.'/../../../Fragment/Home_menu.php';
+?>
 <div id='message'>
-		<p>
-		नमस्ते,
+	<div>
+		<div id='home-message'>
+			<p>नमस्ते,</p>
+			<p>
+				मैं <?php link_xurl('about_me', 'उज्ज्वल सिंह') ?> हूँ।
+			</p>
+			<p>
+				यहाँ मैं अपने विचार, निष्कर्ष और सुझाव साझा करता हूँ।
+			</p>
+			<p>
+				मैं उन्हें अधिक सही और उपयोगी बनाने का प्रयास करता हूँ।
+			</p>
+			<p>
+				नई जानकारी मिलने पर इनमें बदलाव हो सकते हैं।
+			</p>
+			<p>
+				मैं <?php link_xurl('world/philosophy/life', 'जीवन') ?>, <?php link_xurl('world/philosophy/death', 'मृत्यु') ?> और <?php link_xurl('world/philosophy/god', 'ईश्वर') ?> से लेकर समसामयिक विषयों तक लिखता हूँ।
+			</p>
+			<p>
+				मैं कंप्यूटर और तकनीक के बारे में भी लिखता हूँ।
+			</p>
+			<p>
+				नीचे दिए गए सामाजिक माध्यमों पर ‘Ujnotes’ को फ़ॉलो करें।
+			</p>
+		</div>
+		<div id='profile-image-container' class='message_leave'>
+			<a id='profile-image' href='#'>
+				<img src='/photo.jpg' alt='लेखक की तस्वीर'>
+			</a>
+		</div>
+	</div>
+</div>
+<div class='center' id='content-body-separator'></div>
+<div class='message_center_div' id='home-menu'>
+	<section class="home-menu-branch" aria-label="<?php echo htmlspecialchars(getComponentLabel('world')) ?>">
+		<?php home_menu_render_branch('world'); ?>
+	</section>
+	<section class="home-menu-branch" aria-label="<?php echo htmlspecialchars(getComponentLabel('computer')) ?>">
+		<?php home_menu_render_branch('computer'); ?>
+	</section>
+	<p id='home-image-disclaimer'>
+		इस पृष्ठ की कुछ छवियाँ उनके संबंधित स्वामियों की कॉपीराइट हैं। वे सीमित, गैर-व्यावसायिक, सूचनात्मक संदर्भ में उचित उपयोग (fair use) के अंतर्गत यहाँ दी गई हैं। कोई स्वामित्व दावा नहीं किया जाता।
 	</p>
-	<p>
-		मैं <a class="content-link XURL" href="/about_me" data-target="about_me" data-title="उज्ज्वल सिंह">उज्ज्वल सिंह</a> हूँ।
-	</p>
-	<p>
-		यहाँ मैं अपने विचार, निष्कर्ष और सुझाव साझा करता हूँ।
-	</p>
-	<p>
-		मैं उन्हें अधिक सही और उपयोगी बनाने का प्रयास करता हूँ।
-	</p>
-	<p>
-		नई जानकारी मिलने पर इनमें बदलाव हो सकते हैं।
-	</p>
-	<p>
-		मैं <a class="content-link XURL" href="/world/philosophy/life" data-target="world/philosophy/life" data-title="जीवन">जीवन</a>, <a class="content-link XURL" href="/world/philosophy/death" data-target="world/philosophy/death" data-title="मृत्यु">मृत्यु</a> और <a class="content-link XURL" href="/world/philosophy/god" data-target="world/philosophy/god" data-title="ईश्वर">ईश्वर</a> से लेकर समसामयिक विषयों तक लिखता हूँ।
-	</p>
-	<p>
-		मैं कंप्यूटर और तकनीक के बारे में भी लिखता हूँ।
-	</p>
-	<p>
-		नीचे दिए गए सामाजिक माध्यमों पर ‘Ujnotes’ को फ़ॉलो करें।
-	</p>
-	<div id='content-body-separator' class='center'></div>
-	<?php link_xurl('world', 'संसार') ?>
-	<?php group_image('page-list', 0, ['world/philosophy', 'दर्शन']); ?>
-	<?php // '/universe' 'ब्रह्मांड'
-// 'world/reality' 'वास्तविकता' ?>
-	<?php link_xurl('computer', 'कंप्यूटर') ?>
-	<?php group_image('page-list', 1, ['computer/os', 'ऑपरेटिंग सिस्टम', '//ujnotes.com/computer/os'], ['computer/program', 'प्रोग्राम', '//ujnotes.com/computer/program'], ['computer/programming', 'प्रोग्रामिंग', '//ujnotes.com/computer/programming'], ['computer/game', 'खेल']); ?>
-
+</div>
+<div id='fb_components'>
+	<?php require('../HTML/Fragment/Component_FB_buttons.php') ?>
 </div>
