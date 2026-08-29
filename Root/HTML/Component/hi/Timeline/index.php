82732c70acc703d36b2cab88cb79c420ea9973a0
Ujjwal Singh <ujjwalsingh@outlook.com>
2026-08-29 14:47:51 +0530

    Align Timeline dashes by giving dates a fixed-width Consolas column.

--- Root/HTML/Component/hi/Timeline/index.php
+++ Root/HTML/Component/hi/Timeline/index.php
@@ -4,32 +4,32 @@
 	</p>
 	<h3>हाल की मुख्य बातें</h3>
 	<ul class="list-bullet content-list">
-		<li><div>27 Aug 2026 — <a class="content-link XURL" href="/computer/algorithm/binary_search" data-target="computer/algorithm/binary_search" data-title="द्विआधारी खोज">द्विआधारी खोज</a> प्रकाशित; Computer, Game, Technology, कृत्रिम बुद्धिमत्ता और मशीन लर्निंग के आवरण चित्र नए किए</div></li>
-		<li><div>26 Aug 2026 — होमपेज ट्री का पुनर्गठन, ताकि एल्गोरिथ्म, प्रोग्राम और कृत्रिम बुद्धिमत्ता Computer के अंतर्गत रहें</div></li>
-		<li><div>20 Aug 2026 — <a class="content-link XURL" href="/computer" data-target="computer" data-title="कंप्यूटर">कंप्यूटर</a>, <a class="content-link XURL" href="/computer/os" data-target="computer/os" data-title="ओएस">ओएस</a>, <a class="content-link XURL" href="/computer/program" data-target="computer/program" data-title="प्रोग्राम">प्रोग्राम</a>, <a class="content-link XURL" href="/computer/programming" data-target="computer/programming" data-title="प्रोग्रामिंग">प्रोग्रामिंग</a> और <a class="content-link XURL" href="/computer/technology" data-target="computer/technology" data-title="प्रौद्योगिकी">प्रौद्योगिकी</a> प्रकाशित, हिंदी कंप्यूटर लेख सहित</div></li>
-		<li><div>12 Aug 2026 — खुलने और सिमटने वाला होमपेज टाइल वृक्ष जोड़ा</div></li>
-		<li><div> 3 Aug 2026 — <a class="content-link XURL" href="/world/philosophy/hindu" data-target="world/philosophy/hindu" data-title="हिंदू">हिंदू</a> का हिंदी अनुवाद प्रकाशित</div></li>
-		<li><div>28 Jul 2026 — <a class="content-link XURL" href="/world/philosophy/hindu" data-target="world/philosophy/hindu" data-title="हिंदू">हिंदू</a> प्रकाशित</div></li>
+		<li><div><span class='date'>27 Aug 2026</span> — <a class="content-link XURL" href="/computer/algorithm/binary_search" data-target="computer/algorithm/binary_search" data-title="द्विआधारी खोज">द्विआधारी खोज</a> प्रकाशित; Computer, Game, Technology, कृत्रिम बुद्धिमत्ता और मशीन लर्निंग के आवरण चित्र नए किए</div></li>
+		<li><div><span class='date'>26 Aug 2026</span> — होमपेज ट्री का पुनर्गठन, ताकि एल्गोरिथ्म, प्रोग्राम और कृत्रिम बुद्धिमत्ता Computer के अंतर्गत रहें</div></li>
+		<li><div><span class='date'>20 Aug 2026</span> — <a class="content-link XURL" href="/computer" data-target="computer" data-title="कंप्यूटर">कंप्यूटर</a>, <a class="content-link XURL" href="/computer/os" data-target="computer/os" data-title="ओएस">ओएस</a>, <a class="content-link XURL" href="/computer/program" data-target="computer/program" data-title="प्रोग्राम">प्रोग्राम</a>, <a class="content-link XURL" href="/computer/programming" data-target="computer/programming" data-title="प्रोग्रामिंग">प्रोग्रामिंग</a> और <a class="content-link XURL" href="/computer/technology" data-target="computer/technology" data-title="प्रौद्योगिकी">प्रौद्योगिकी</a> प्रकाशित, हिंदी कंप्यूटर लेख सहित</div></li>
+		<li><div><span class='date'>12 Aug 2026</span> — खुलने और सिमटने वाला होमपेज टाइल वृक्ष जोड़ा</div></li>
+		<li><div><span class='date'>3 Aug 2026</span> — <a class="content-link XURL" href="/world/philosophy/hindu" data-target="world/philosophy/hindu" data-title="हिंदू">हिंदू</a> का हिंदी अनुवाद प्रकाशित</div></li>
+		<li><div><span class='date'>28 Jul 2026</span> — <a class="content-link XURL" href="/world/philosophy/hindu" data-target="world/philosophy/hindu" data-title="हिंदू">हिंदू</a> प्रकाशित</div></li>
 	</ul>
 	<h3>पूर्व सार्वजनिक साइट</h3>
 	<ul class="list-bullet content-list">
-		<li><div>15 Mar 2017 — <a class="content-link XURL" href="/computer/game/doom/2016/demo/issue/vulkan_crash" data-target="computer/game/doom/2016/demo/issue/vulkan_crash" data-title="वल्कन क्रैश">वल्कन क्रैश</a> प्रकाशित</div></li>
-		<li><div>09 Feb 2017 — <a class="content-link XURL" href="/world/philosophy/solution" data-target="world/philosophy/solution" data-title="समाधान">समाधान</a> प्रकाशित</div></li>
-		<li><div>09 Jan 2017 — <a class="content-link XURL" href="/world/philosophy/problem" data-target="world/philosophy/problem" data-title="समस्या">समस्या</a> प्रकाशित</div></li>
-		<li><div>11 Dec 2016 — <a class="content-link XURL" href="/world/philosophy/god/no_god" data-target="world/philosophy/god/no_god" data-title="कोई ईश्वर नहीं">कोई ईश्वर नहीं</a> प्रकाशित</div></li>
-		<li><div>16 Nov 2016 — <a class="content-link XURL" href="/world/philosophy/god" data-target="world/philosophy/god" data-title="ईश्वर">ईश्वर</a> प्रकाशित</div></li>
-		<li><div> 7 Nov 2016 — <a class="content-link XURL" href="/world/philosophy/death" data-target="world/philosophy/death" data-title="मृत्यु">मृत्यु</a> प्रकाशित</div></li>
-		<li><div> 5 Nov 2016 — <a class="content-link XURL" href="/world/philosophy/life" data-target="world/philosophy/life" data-title="जीवन">जीवन</a> प्रकाशित</div></li>
-		<li><div> 3 Nov 2016 — <a class="content-link XURL" href="/world/philosophy" data-target="world/philosophy" data-title="दर्शन">दर्शन</a> प्रकाशित</div></li>
+		<li><div><span class='date'>15 Mar 2017</span> — <a class="content-link XURL" href="/computer/game/doom/2016/demo/issue/vulkan_crash" data-target="computer/game/doom/2016/demo/issue/vulkan_crash" data-title="वल्कन क्रैश">वल्कन क्रैश</a> प्रकाशित</div></li>
+		<li><div><span class='date'>09 Feb 2017</span> — <a class="content-link XURL" href="/world/philosophy/solution" data-target="world/philosophy/solution" data-title="समाधान">समाधान</a> प्रकाशित</div></li>
+		<li><div><span class='date'>09 Jan 2017</span> — <a class="content-link XURL" href="/world/philosophy/problem" data-target="world/philosophy/problem" data-title="समस्या">समस्या</a> प्रकाशित</div></li>
+		<li><div><span class='date'>11 Dec 2016</span> — <a class="content-link XURL" href="/world/philosophy/god/no_god" data-target="world/philosophy/god/no_god" data-title="कोई ईश्वर नहीं">कोई ईश्वर नहीं</a> प्रकाशित</div></li>
+		<li><div><span class='date'>16 Nov 2016</span> — <a class="content-link XURL" href="/world/philosophy/god" data-target="world/philosophy/god" data-title="ईश्वर">ईश्वर</a> प्रकाशित</div></li>
+		<li><div><span class='date'>7 Nov 2016</span> — <a class="content-link XURL" href="/world/philosophy/death" data-target="world/philosophy/death" data-title="मृत्यु">मृत्यु</a> प्रकाशित</div></li>
+		<li><div><span class='date'>5 Nov 2016</span> — <a class="content-link XURL" href="/world/philosophy/life" data-target="world/philosophy/life" data-title="जीवन">जीवन</a> प्रकाशित</div></li>
+		<li><div><span class='date'>3 Nov 2016</span> — <a class="content-link XURL" href="/world/philosophy" data-target="world/philosophy" data-title="दर्शन">दर्शन</a> प्रकाशित</div></li>
 	</ul>
 	<h3>पहला संस्करण प्रकाशित</h3>
 	<ul class="list-bullet content-list">
-		<li><div>29 Feb 2016 — वर्तमान साइट का पहला संस्करण प्रकाशित — Cutie लेआउट</div></li>
-		<li><div>11 Jan 2016 — सार्वजनिक होमपेज का सबसे पुराना इंटरनेट आर्काइव अभिलेख: ओएस, प्रोग्राम और प्रोग्रामिंग वाली संक्षिप्त स्वागत पृष्ठ (<a class="content-link" href="https://web.archive.org/web/20160111232234/http://ujnotes.com/" target="_blank" rel="noopener noreferrer">इंटरनेट आर्काइव</a>)</div></li>
+		<li><div><span class='date'>29 Feb 2016</span> — वर्तमान साइट का पहला संस्करण प्रकाशित — Cutie लेआउट</div></li>
+		<li><div><span class='date'>11 Jan 2016</span> — सार्वजनिक होमपेज का सबसे पुराना इंटरनेट आर्काइव अभिलेख: ओएस, प्रोग्राम और प्रोग्रामिंग वाली संक्षिप्त स्वागत पृष्ठ (<a class="content-link" href="https://web.archive.org/web/20160111232234/http://ujnotes.com/" target="_blank" rel="noopener noreferrer">इंटरनेट आर्काइव</a>)</div></li>
 	</ul>
 	<h3>डोमेन पंजीकृत</h3>
 	<ul class="list-bullet content-list">
-		<li><div> 3 Mar 2014 — डोमेन ujnotes.com पंजीकृत</div></li>
+		<li><div><span class='date'>3 Mar 2014</span> — डोमेन ujnotes.com पंजीकृत</div></li>
 	</ul>
 	<div id='content-body-separator' class='center'></div>
 	<p>
